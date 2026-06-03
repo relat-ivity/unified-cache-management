82330e432b175c30cef3a8e56616516c90f3bd90
黄可欣 <2218887102@qq.com>
2026-06-03 15:59:36 +0800

    Revert "修复网卡问题"
    This reverts commit 3ef602f04198809dfb44e5a5277c17fa0a8415ac.

--- ucm/store/cache/cc/cache_store.cc
+++ ucm/store/cache/cc/cache_store.cc
@@ -49,11 +49,9 @@ public:
             UC_ERROR("Failed to check config params: {}.", s);
             return s;
         }
-        if (config.useGdr && config.deviceId >= 0 && !config.gpuKvBufferAddrs.empty()) {
+        if (config.useGdr) {
             s = SetupGdrMrOnlyExperiment(config);
             if (s.Failure()) [[unlikely]] { return s; }
-        } else if (config.useGdr) {
-            config.useGdr = false;
         }
         s = bufferMgr_.Setup(config);
         if (s.Failure()) [[unlikely]] {
