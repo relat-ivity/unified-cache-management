f2d34e4c6838d755379b9b8b1eba4169e5967434
黄可欣 <2218887102@qq.com>
2026-06-05 09:13:58 +0800

    脚本

---
+++ examples/deployments/scripts/vllm/run_deepseek_v4_flash_single_node.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+export CUDA_VISIBLE_DEVICES=0,1,2,3 # ,4,5,6,7
+export DEVICE_GDR_NICS=mlx5_0,mlx5_2,mlx5_4,mlx5_6 # ,mlx5_8,mlx5_10,mlx5_12,mlx5_14
+
+LOG_FILE="./vllm_deepseek_v4_flash_$(date +%Y%m%d_%H%M%S).log"
+
+vllm serve /home/models/DeepSeek-V4-Flash \
+  --trust-remote-code \
+  --kv-cache-dtype fp8 \
+  --block-size 256 \
+  --enable-expert-parallel \
+  --tensor-parallel-size 4 \
+  --max-num-batched-tokens 8192 \
+  --max-model-len 8192 \
+  --no-disable-hybrid-kv-cache-manager \
+  --host 0.0.0.0 \
+  --port 8000 \
+  --kv-transfer-config \
+  '{
+    "kv_connector": "UCMConnector",
+    "kv_connector_module_path": "ucm.integration.vllm.ucm_connector",
+    "kv_role": "kv_both",
+    "kv_connector_extra_config": {"UCM_CONFIG_FILE": "./examples/ucm_deepseek_v4_flash_cache_posix_local.yaml"}
+  }' \
+2>&1 | tee "./vllm_deepseek_v4_flash_$(date +%Y%m%d_%H%M%S).log"
